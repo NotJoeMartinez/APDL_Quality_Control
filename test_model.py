476629fe92d7421dfbbd5166e148107d94488c6f
notjoemartinez <notjoemartinez@protonmail.com>
2021-03-23 00:59:42 -0500

    test_all_imgs() of test_model.py now returns a Pandas dataframe
    This should make it easier to do math and stuff on tests instead of just eyeballing it like normal.

--- test_model.py
+++ test_model.py
@@ -14,6 +14,8 @@ import matplotlib.image as mpimg
 
 import subprocess
 
+import pandas as pd 
+
 # load model
 model = keras.models.load_model("models/03_22_3:36PM")
 # model.summary()
@@ -22,17 +24,31 @@ img_height = 180
 img_width = 180
 class_names = ['broken wire', 'glue', 'good', 'missing wire', 'unknown debris']
 test_data_paths = "datasets/testing/test_all_features"
+rubric_dict =  {
+  "broken_wire": "broken wire",
+  "glue": "glue",
+  "good": "good",
+  "missing_wire": "missing wire",
+  "unknown_debris": "unknown debris"
+}
 
+# finds image names out of the training dataset then returns their parent directory 
 def find_image_label(img_name, file_path="datasets/training/all_features", ):
   foo = subprocess.check_output("find {} -name {}".format(file_path,img_name) , shell=True)
   parent_dir = re.findall("\/(\w*)\/", str(foo))
+  
   try:
-    return parent_dir[1]
+      return "{}".format(parent_dir[1])
   except IndexError:
-    return "Unkown"
+    pass
 
-
+"""
+Itterates through all test images, prints predictions, confidence levels 
+and wheather it actually  predicted it correctly it should return a pandas dataframe
+"""
 def test_all_imgs():
+
+  pandas_data = []
   # makes list of test data paths
   data_paths = []
   for root, dirs, files in os.walk(test_data_paths):
@@ -41,6 +57,9 @@ def test_all_imgs():
   
   # Makes preditctions of every image in the data paths list
   for count, test_img in enumerate(data_paths):
+    temp_data = []
+
+    # turn test_img into a preprocessed keras object or something
     img = keras.preprocessing.image.load_img(
         test_img, target_size=(img_height, img_width)
     )
@@ -51,15 +70,38 @@ def test_all_imgs():
     predictions = model.predict(img_array)
     score = tf.nn.softmax(predictions[0])
     
+    # declare some more varabables 
     test_file_name = re.findall("\/.*\/(.*\.png)",test_img)
-    try:
-      print(
-        "Prediction: {}, Truth: {}, Percent Confidence: {:.2f}".format(class_names[np.argmax(score)], find_image_label(test_file_name[0]), 100 * np.max(score))
-      )
-    except IndexError:
-      pass
+    model_prediction = class_names[np.argmax(score)]
 
-test_all_imgs()
+    # check stuff against reality 
+    try:
+      true_file_label = find_image_label(test_file_name[0])
+      if rubric_dict[true_file_label] == model_prediction:
+        prediction_truth = "True" 
+      else:
+        prediction_truth = "False" 
+      
+      # print(
+      #   "Prediction: {}, Truth: {}, Percent Confidence: {:.2f}".format(model_prediction, 
+      #    prediction_truth, 
+      #   100 * np.max(score))
+      # )
+
+      temp_data.append(model_prediction) 
+      temp_data.append(prediction_truth) 
+      temp_data.append(100 * np.max(score)) 
+
+      pandas_data.append(temp_data)
+
+    except (IndexError, KeyError):
+      pass
+  return pandas_data 
+
+yeet = test_all_imgs()
+df = pd.DataFrame(yeet, columns = ['prediction','prediction_truth','confidence'])
+print(df)
+
 
 # Builds the plot with images of random images and one image of a broken wire
 def random_test_plot():
