c79433aadb75f0753fefba9d67c5dfe339e0efc4
notjoemartinez <notjoemartinez@protonmail.com>
2021-03-22 23:34:56 -0500

    implemented subprocess to use the find command instead of actually writing good code

--- test_model.py
+++ test_model.py
@@ -1,4 +1,4 @@
-import os, random, sqlite3, pathlib
+import os, random, sqlite3, pathlib, re
 import PIL
 
 import numpy as np
@@ -12,6 +12,7 @@ from tensorflow.keras.models import Sequential
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+import subprocess
 
 # load model
 model = keras.models.load_model("models/03_22_3:36PM")
@@ -20,36 +21,45 @@ model = keras.models.load_model("models/03_22_3:36PM")
 img_height = 180
 img_width = 180
 class_names = ['broken wire', 'glue', 'good', 'missing wire', 'unknown debris']
+test_data_paths = "datasets/testing/test_all_features"
 
-fpath = input("Testing Path: ")
+def find_image_label(img_name, file_path="datasets/training/all_features", ):
+  foo = subprocess.check_output("find {} -name {}".format(file_path,img_name) , shell=True)
+  parent_dir = re.findall("\/(\w*)\/", str(foo))
+  try:
+    return parent_dir[1]
+  except IndexError:
+    return "Unkown"
+
 
 def test_all_imgs():
-  # makes a list of data paths 
+  # makes list of test data paths
   data_paths = []
-  for root, dirs, files in os.walk(fpath):
+  for root, dirs, files in os.walk(test_data_paths):
     for name in files:
       data_paths.append(os.path.join(root, name))
-    
-  # itterates 
+  
+  # Makes preditctions of every image in the data paths list
   for count, test_img in enumerate(data_paths):
     img = keras.preprocessing.image.load_img(
         test_img, target_size=(img_height, img_width)
     )
+
+    # turn image to image array an made predicitons using the model
     img_array = keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0) 
     predictions = model.predict(img_array)
     score = tf.nn.softmax(predictions[0])
-
-    print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-        )
-
+    
+    test_file_name = re.findall("\/.*\/(.*\.png)",test_img)
+    try:
+      print(
+        "Prediction: {}, Truth: {}, Percent Confidence: {:.2f}".format(class_names[np.argmax(score)], find_image_label(test_file_name[0]), 100 * np.max(score))
+      )
+    except IndexError:
+      pass
 
 test_all_imgs()
-
-
-
 
 # Builds the plot with images of random images and one image of a broken wire
 def random_test_plot():
