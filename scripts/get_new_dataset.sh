fad0d146376c81055f9734a6019005740e5e8427
notjoemartinez <notjoemartinez@protonmail.com>
2021-05-07 15:34:26 -0500

    added shell script to rename images and convert them to jpg

--- scripts/get_new_dataset.sh
+++
@@ -1,4 +0,0 @@
-wget "https://github.com/NotJoeMartinez/APDL_Quality_Control/blob/main/datasets/original.tar.gz?raw=true"
-mv 'original.tar.gz?raw=true' original.tar.gz
-tar -zxvf original.tar.gz
-find original -type f -name '\.*' -delete
--- scripts/remove_ds_store.sh
+++
@@ -1,2 +0,0 @@
-# Pesky MacOS hidden files
-find . -name '.DS_Store' -type f -delete
